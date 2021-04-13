4c385ec753d2c41c5a52994aecc748a74d960024
unknown <edward2000ed@gmail.com>
2021-04-13 09:04:20 +0530

    finished game basic mechanics

--- script.js
+++ script.js
@@ -1,14 +1,33 @@
 let bird;
+let pipes = [];
 
 function setup() {
-    createCanvas(400,600);
+    createCanvas(600,600);
     bird = new Bird();
+    pipes.push(new Pipe);
 }
 
 function draw() {
     background(0);
     bird.show();
     bird.update();
+
+    if(frameCount % 100 == 0) {
+        pipes.push(new Pipe);
+    }
+    pipes.forEach(pipe => {
+        pipe.show();
+        pipe.update();
+
+        if(pipe.hits(bird)) {
+            console.log("hit");
+        }
+    });
+
+    if(pipes[0].offscreen()) {
+        pipes.shift();
+    };
+    
 }
 
 function keyPressed() {
